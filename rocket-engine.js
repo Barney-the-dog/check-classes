376577d735a8955d43a0c10b0a96a9e22297a015
Irina Ashurova <you-ve_got@mail.ru>
2023-04-18 21:19:57 -0400

    partially

--- rocket-engine.js
+++ rocket-engine.js
@@ -1,7 +1,10 @@
 class RocketEngine {
   constructor(specificImpulse, rocketPropellant) {
-    
+    this.specificImpulse = specificImpulse;
+    this.rocketPropellant = rocketPropellant;
   }
+
+
 }
 
 module.exports = RocketEngine;
